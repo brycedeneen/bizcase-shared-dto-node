af31ea247ea3a0df6acc109c63e3b3dcbedda483
Bryce D <brycedeneen@gmail.com>
2022-08-13 13:07:01 -0400

    permissions

--- dist/enums/auth/permissions.enum.d.ts
+++ dist/enums/auth/permissions.enum.d.ts
@@ -1,3 +1,19 @@
 export declare enum PermissionsENUM {
-    GETCOMPANY = "getcompany"
+    GETCOMPANY = "getcompany",
+    MODIFYCOMPANY = "modifycompany",
+    CREATECOMPANY = "createcompany",
+    GETTHEME = "gettheme",
+    MODIFYTHEME = "modifytheme",
+    GETDEVELOPMENTCYCLE = "getdevelopmentcycle",
+    MODIFYDEVELOPMENTCYCLE = "modifydevelopmentcycle",
+    GETSCORINGCRITERIA = "getscoringcriteria",
+    MODIFYSCORINGCRITERIA = "modifyscoringcriteria",
+    GETSEGMENT = "getsegment",
+    MODIFYSEGMENT = "modifysegment",
+    GETTEAM = "getteam",
+    MODIFYTEAM = "modifyteam",
+    GETFTE = "getfte",
+    MODIFYFTE = "modifyfte",
+    GETCONCEPT = "getconcept",
+    MODIFYCONCEPT = "modifyconcept"
 }
--- dist/enums/auth/permissions.enum.js
+++ dist/enums/auth/permissions.enum.js
@@ -4,5 +4,21 @@ exports.PermissionsENUM = void 0;
 var PermissionsENUM;
 (function (PermissionsENUM) {
     PermissionsENUM["GETCOMPANY"] = "getcompany";
+    PermissionsENUM["MODIFYCOMPANY"] = "modifycompany";
+    PermissionsENUM["CREATECOMPANY"] = "createcompany";
+    PermissionsENUM["GETTHEME"] = "gettheme";
+    PermissionsENUM["MODIFYTHEME"] = "modifytheme";
+    PermissionsENUM["GETDEVELOPMENTCYCLE"] = "getdevelopmentcycle";
+    PermissionsENUM["MODIFYDEVELOPMENTCYCLE"] = "modifydevelopmentcycle";
+    PermissionsENUM["GETSCORINGCRITERIA"] = "getscoringcriteria";
+    PermissionsENUM["MODIFYSCORINGCRITERIA"] = "modifyscoringcriteria";
+    PermissionsENUM["GETSEGMENT"] = "getsegment";
+    PermissionsENUM["MODIFYSEGMENT"] = "modifysegment";
+    PermissionsENUM["GETTEAM"] = "getteam";
+    PermissionsENUM["MODIFYTEAM"] = "modifyteam";
+    PermissionsENUM["GETFTE"] = "getfte";
+    PermissionsENUM["MODIFYFTE"] = "modifyfte";
+    PermissionsENUM["GETCONCEPT"] = "getconcept";
+    PermissionsENUM["MODIFYCONCEPT"] = "modifyconcept";
 })(PermissionsENUM = exports.PermissionsENUM || (exports.PermissionsENUM = {}));
 //# sourceMappingURL=permissions.enum.js.map
